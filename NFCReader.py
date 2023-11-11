5f9de14c65c5ffdb285dff991ca037ca7797ef0e
kenya_k66 <sasaki.kenya.21@shizuoka.ac.jp>
2023-11-11 17:44:51 +0900

    fixed get_name function

--- NFCReader.py
+++ NFCReader.py
@@ -18,14 +18,32 @@ class NFCReader:
         bc2 = nfc.tag.tt3.BlockCode(1, service=0)
         block_data = tag.read_without_encryption([sc1], [bc1, bc2])
         student_id = block_data[1:9].decode("utf-8")
-        print("学生番号: " + student_id)
+        print("学籍番号: " + student_id)
         return student_id
+
+    def get_name(self, tag):
+        try:
+            # 修正が必要ならば、以下のサービスコードとブロックコードを確認して調整してください
+            idm, pmm = tag.polling(system_code=0xfe00)
+            tag.idm, tag.pmm, tag.sys = idm, pmm, 0xfe00
+            sc = nfc.tag.tt3.ServiceCode(106, 0x0b) 
+            bc = nfc.tag.tt3.BlockCode(1, service=0)
+            name_data = tag.read_without_encryption([sc], [bc])
+            student_name = name_data.decode('shift_jis')
+            print("氏名: " + student_name)
+            return student_name
+        except nfc.tag.tt3.Type3TagCommandError as e:
+            print(f"NFC タグの読み取りエラー: {e}")
+            return None
+
 
     def on_connect(self, tag):
         print("[*] connected:", tag)
         student_id = self.get_id(tag)
+        student_name = self.get_name(tag)
         # 音声合成
-        text_to_speech(f"学籍番号 {student_id}。お疲れ様です！今日も一日頑張りましたね！")
+        text_to_speech(f"学籍番号 {student_id}。{student_name}さん、 お疲れ様です！今日も一日頑張りましたね！")
+        #text_to_speech(f"学籍番号 {student_id}、お疲れ様です！今日も一日頑張りましたね！")
         return True
 
     def on_release(self, tag):
